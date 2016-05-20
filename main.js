bd9cc72978b518ad2bea602ba6535b447aab590b
yt-harada <yt-harada@kddi-tech.com>
2016-05-20 14:00:46 +0900

    タブをスペースに変更 GCM側の対応

--- main.js
+++ main.js
@@ -2,57 +2,66 @@ var isLogin = false;
 
 /** ページ読み込み処理 */
 window.addEventListener('load', function() {
-	// WebPush対応ブラウザかチェックする
-	initialize(initializeResult);
+    // WebPush対応ブラウザかチェックする
+    initialize(initializeResult);
+    getSubscription(isRegister);
 
 
 });
 
 /** 全てのボタンを無効にする */
 function unSupported() {
-	var all = document.all;
-	for (i = 0; i < all.length; i++) {
-		var el= all(i);
-		if (el.tagName == "INPUT") {
-			if (el.type == "button") {
-				el.disabled = true;
-			}
-		}
-	}
+    var all = document.all;
+    for (i = 0; i < all.length; i++) {
+        var el= all(i);
+        if (el.tagName == "INPUT") {
+            if (el.type == "button") {
+                el.disabled = true;
+            }
+        }
+    }
 }
 
 
 /** ログイン・ログアウトボタンを押された時の処理 */
 function onLogin(btn) {
-	if (isLogin == false) {
-		btn.value ='ログアウト';
-		isLogin= true;
-	} else {
-		btn.value ='au IDにログイン';
-		isLogin= false;
-	}
+    if (isLogin == false) {
+        btn.value ='ログアウト';
+        isLogin= true;
+    } else {
+        btn.value ='au IDにログイン';
+        isLogin= false;
+    }
 }
 
 /** 購読・解約ボタンを押された時の処理 */
 function buyTopic(btn, topic) {
-	if (btn.value == '購読') {
-		btn.value = '解約';
-	} else {
-		btn.value = '購読';
-	}
+    if (btn.value == '購読') {
+        btn.value = '解約';
+    } else {
+        btn.value = '購読';
+        registSubscription(registerResult);
+    }
 }
 
 
 
 function initializeResult(result) {
-	// WebPush対応ブラウザであればSWの登録等の処理をする
-	// WebPush非対応ブラウザであれば全てのボタンを無効にして操作できないようにする
-	if (result == true) {
-		document.getElementById('support').innerText = 'このブラウザはWebPushの対応ブラウザです';
-	} else {
-		document.getElementById('support').innerText = 'このブラウザはWebPushに対応していないためご利用できません';
-		unSupported();
-	}
+    // WebPush対応ブラウザであればSWの登録等の処理をする
+    // WebPush非対応ブラウザであれば全てのボタンを無効にして操作できないようにする
+    if (result == true) {
+        document.getElementById('support').innerText = 'このブラウザはWebPushの対応ブラウザです';
+    } else {
+        document.getElementById('support').innerText = 'このブラウザはWebPushに対応していないためご利用できません';
+        unSupported();
+    }
 }
 
+function registerResult(result) {
+    console.log(result);
+}
+
+function isRegister(result) {
+    console.log(result);
+}
 
--- opo.js
+++ opo.js
@@ -4,6 +4,10 @@ var CHROME_SUPPORT_VER = 42;
 var SAFARI_SUPPORT_VER = 500;
 
 
+/** 表示しているブラウザがSafariか否か（接続先のPushサーバーの選別に使用） */
+var IS_SAFARI = false;
+
+
 /***************************************************************
 　OPOライブラリの初期化要求.
 　
@@ -12,77 +16,104 @@ var SAFARI_SUPPORT_VER = 500;
 　WebPush非対応ブラウザの場合にはコールバック関数の引数にfalseを設定する.
 ***************************************************************/
 function initialize(result) {
-	// WebPush対応ブラウザか否かの返却値
-	var isSupport = false;
+    // WebPush対応ブラウザか否かの返却値
+    var isSupport = false;
 
-	// UAを取得
-	var agent = navigator.userAgent; 
-	console.log('userAgent=' + agent);
+    // UAを取得
+    var agent = navigator.userAgent; 
+    console.log('userAgent=' + agent);
 
-	// UAの判定式
-	if (navigator.platform.search('Mac') != -1) { 
-		console.log('Mac');
-		if ((agent.search('Chrome') != -1) && (agent.search('OPR') == -1)) {
-			console.log('chrome');
-			isSupport = checkBrowserVersion('Chrome', CHROME_SUPPORT_VER);
-		} else if ((agent.search('Safari') != -1) && (agent.search('Chrome') == -1) && (agent.search('OPR') == -1) && (agent.search('Presto') == -1)) {
-			console.log('Safari');
-			isSupport = true;
-		}
-	} else if (agent.search('Windows') != -1) {
-		console.log('Windows');
-		if ((agent.search('Chrome') != -1) && (agent.search('OPR') == -1)) {
-			console.log('chrome');
-			isSupport = checkBrowserVersion('Chrome', CHROME_SUPPORT_VER);
-		}
-	} else if (agent.search('Android') != -1) {
-		console.log('Android');
-		if ((agent.search('Chrome') != -1) && (agent.search('OPR') == -1)) {
-			console.log('chrome');
-			isSupport = checkBrowserVersion('Chrome', CHROME_SUPPORT_VER);
-		}
-	} else {
-		console.log('対象外のブラウザです');
-	}
+    // UAの判定式
+    if (navigator.platform.search('Mac') != -1) { 
+        console.log('Mac');
+        if ((agent.search('Chrome') != -1) && (agent.search('OPR') == -1)) {
+            console.log('chrome');
+            isSupport = checkBrowserVersion('Chrome', CHROME_SUPPORT_VER);
+        } else if ((agent.search('Safari') != -1) && (agent.search('Chrome') == -1) && (agent.search('OPR') == -1) && (agent.search('Presto') == -1)) {
+            console.log('Safari');
+            isSupport = true;
+            IS_SAFARI = true;
+        }
+    } else if (agent.search('Windows') != -1) {
+        console.log('Windows');
+        if ((agent.search('Chrome') != -1) && (agent.search('OPR') == -1)) {
+            console.log('chrome');
+            isSupport = checkBrowserVersion('Chrome', CHROME_SUPPORT_VER);
+        }
+    } else if (agent.search('Android') != -1) {
+        console.log('Android');
+        if ((agent.search('Chrome') != -1) && (agent.search('OPR') == -1)) {
+            console.log('chrome');
+            isSupport = checkBrowserVersion('Chrome', CHROME_SUPPORT_VER);
+        }
+    } else {
+        console.log('対象外のブラウザです');
+    }
 
-	if (isSupport == true) {
-		// サポート対象のブラウザであればServiceWorkerの登録を試みる
-		// ServiceWorkerの登録結果も込みで初期化処理の成否判定とする
-		registServiceWorker(result);
-	} else {
-		// 非サポートのブラウザであれば引数にfalseを指定してコールバック関数を呼び出す
-		result(false);
-	}
+    if (isSupport == true) {
+        // サポート対象のブラウザであればServiceWorkerの登録を試みる
+        // ServiceWorkerの登録結果も込みで初期化処理の成否判定とする
+        registServiceWorker(result);
+    } else {
+        // 非サポートのブラウザであれば引数にfalseを指定してコールバック関数を呼び出す
+        result(false);
+    }
 }
 
 
 /***************************************************************
 　Subscriptionの解除
+　
+　Chromeの場合にはGCMに対してSubscriptionの解除要求を行う.
+　Safariの場合にはAPNSに対してSubscriptionの解除要求を行う.
 ***************************************************************/
 function unregistSubscription() {
+    if (IS_SAFARI) {
+    } else {
+        unregisterGCM();
+    }
 }
 
 
 /***************************************************************
 　Subscriptionの要求
 　
-　Pushサーバーに対してSubscriptionの登録を行う.
-　登録に成功した場合にはコールバック関数の引数にSubscriptionIDを設定する.
-　登録に失敗した場合にはコールバック関数の引数にnullを設定する.
+　Chromeの場合にはGCMに対してSubscriptionの登録要求を行う.
+　Safariの場合にはAPNSに対してSubscriptionの登録要求を行う.
 ***************************************************************/
 function registSubscription(result) {
+    if (IS_SAFARI) {
+    } else {
+        registerGCM(result);
+    }
 }
 
 
 /***************************************************************
 　Subscriptionの確認.
 　
-　登録済みの場合にはコールバック関数の引数にSubscriptionIDを設定する.
-　未登録の場合にはコールバック関数の引数にnullを設定する.
+　Chromeの場合にはGCMに対してSubscriptionの確認要求を行う.
+　Safariの場合にはAPNSに対してSubscriptionの確認要求を行う.
 ***************************************************************/
 function getSubscription(result) {
+    if (IS_SAFARI) {
+    } else {
+        getSubscriptionGCM(result);
+    }
 }
 
+
+
+/**  Public Function End  **/
+
+
+/***************************************************************
+　
+　＜＜　注意　＞＞
+　ここより下の関数はライブラリ内部で使用する関数のため、
+　サービスサイト側から直接呼び出したときの動作保証はしない。
+　
+***************************************************************/
 
 /***************************************************************
 　UAからバージョンのみ抽出してサポートバージョンかを判定する.
@@ -91,18 +122,18 @@ function getSubscription(result) {
 　false：WebPushをサポートしていないブラウザ.
 ***************************************************************/
 function checkBrowserVersion(searchWord, supportVer) {
-	var ret = false;
-	var agent = navigator.userAgent; 
-	var index = agent.search(searchWord);
-	agent = agent.substr(index + 7);
+    var ret = false;
+    var agent = navigator.userAgent; 
+    var index = agent.search(searchWord);
+    agent = agent.substr(index + 7);
 
-	var browserVer = agent.split('.');
-	console.log(browserVer[0]);
-	if (browserVer[0] >= supportVer) {
-		ret = true;
-	}
+    var browserVer = agent.split('.');
+    console.log(browserVer[0]);
+    if (browserVer[0] >= supportVer) {
+        ret = true;
+    }
 
-	return ret;
+    return ret;
 }
 
 
@@ -113,21 +144,90 @@ function checkBrowserVersion(searchWord, supportVer) {
 　ServiceWorkerの登録に失敗した場合にはコールバック関数の引数にfalseを設定する.
 ***************************************************************/
 function registServiceWorker(result) {
-	var ret = false;
+    var ret = false;
 
-	// ServiceWorkerの登録
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('./sw.js').then(function(registration) {
-			console.log('ServiceWorkerの登録に成功しました');
-			result(true);
-		}).catch(function(err) {
-			console.log('ServiceWorkerの登録に失敗しました');
-			result(false);
-		});
-	} else {
-		console.log('ServiceWorkerのAPIが無効です');
-		result(false);
-	}
+    // ServiceWorkerの登録
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js').then(function(registration) {
+            console.log('ServiceWorkerの登録に成功しました');
+            result(true);
+        }).catch(function(err) {
+            console.log('ServiceWorkerの登録に失敗しました');
+            result(false);
+        });
+    } else {
+        console.log('ServiceWorkerのAPIが無効です');
+        result(false);
+    }
 }
 
+
+
+/***************************************************************
+　Subscriptionの要求
+　
+　GCMに対してSubscriptionの登録を行う.
+　登録に成功した場合にはコールバック関数の引数にSubscriptionIDを設定する.
+　登録に失敗した場合にはコールバック関数の引数にnullを設定する.
+***************************************************************/
+function registerGCM(result) {
+    navigator.serviceWorker.ready.then(function(registration) {
+        registration.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription) {
+            console.log('subscribe 成功');
+            result(subscription);
+        }).catch(function(e) {
+            console.log('subscribe 失敗');
+            result(null);
+        });
+    }).catch(function(e) {
+        console.log('ServiceWorkerがReady状態ではない');
+    });
+}
+
+/***************************************************************
+　Subscriptionの解除
+　
+　GCMに対してSubscriptionの解除を行う.
+***************************************************************/
+function unregisterGCM() {
+    navigator.serviceWorker.ready.then(function(registration) {
+        registration.pushManager.getSubscription().then(function(pushSubscription) {
+            pushSubscription.unsubscribe().then(function() {
+                console.log('unsubscribe 成功');
+            }).catch(function(e) {
+                console.log('unsubscribe 失敗');
+            });
+        }).catch(function(e) {
+            console.log('Subscriptionが取得できない');
+        });
+    }).catch(function(e) {
+        console.log('ServiceWorkerがReady状態ではない');
+    });
+}
+
+/***************************************************************
+　Subscriptionの確認.
+　
+　GCMに対してSubscriptionの登録確認を行う.
+　登録済みの場合にはコールバック関数の引数にSubscriptionIDを設定する.
+　未登録の場合にはコールバック関数の引数にnullを設定する.
+***************************************************************/
+function getSubscriptionGCM(result) {
+    navigator.serviceWorker.ready.then(function(registration) {
+        registration.pushManager.getSubscription().then(function(subscription) {
+            if (pushSubscription.endpoint.indexOf('https://android.googleapis.com/gcm/send') !== 0) {
+                result(pushSubscription.endpoint);
+            } else {
+                console.log('Subscriptionの値がおかしい');
+                result(null);
+            }
+        }).catch(function(e) {
+            console.log('Subscriptionが取得できない');
+            result(null);
+        });
+    }).catch(function(e) {
+        console.log('ServiceWorkerがReady状態ではない');
+        result(null);
+    });
+}
 
